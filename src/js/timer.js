f6ef54be87bed67e43976836382d55ada1efdb12
Muneue SUWA <dongsiku@s.okayama-u.ac.jp>
2021-02-11 20:57:26 +0900

    Add mp3 file

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,16 +1,29 @@
 const TIMER_PARAGRAPH = document.getElementById('timer');
 const START_STOP_BUTTON = document.getElementById('start-stop-btn');
 const RESET_BUTTON = document.getElementById('reset-btn');
+const SOUND_TEST_BUTTON = document.getElementById('sound-test-btn');
 
 let time = 0;
 let runTimer = 0;
 let timerId;
+
+const AUDIO_FILE = new Audio('sound/Doorbell-Ding_Dong02-1.mp3');
+AUDIO_FILE.load();
+// When the audio file is loaded, start to play it.
+AUDIO_FILE.addEventListener('loadedmetadata', () => {
+  START_STOP_BUTTON.disabled = false;
+});
+// When the audio file could not be loaded, appear error message.
+AUDIO_FILE.addEventListener('error', () => {
+  START_STOP_BUTTON.textContent = 'Error: Loading failed';
+});
 
 START_STOP_BUTTON.addEventListener('click', () => {
   if (runTimer == 0) {
     START_STOP_BUTTON.textContent = 'Stop';
     runTimer = 1;
     RESET_BUTTON.disabled = true;
+    SOUND_TEST_BUTTON.disabled = true;
 
     timerId = setInterval(() => {
       updateTimerParagraph(time);
@@ -20,6 +33,7 @@ START_STOP_BUTTON.addEventListener('click', () => {
     START_STOP_BUTTON.textContent = 'Start';
     runTimer = 0;
     RESET_BUTTON.disabled = false;
+    SOUND_TEST_BUTTON.disabled = false;
 
     clearInterval(timerId);
   }
@@ -27,12 +41,16 @@ START_STOP_BUTTON.addEventListener('click', () => {
 
 RESET_BUTTON.addEventListener('click', () => {
   RESET_BUTTON.disabled = true;
+  SOUND_TEST_BUTTON.disabled = true;
 
   time = 0;
   updateTimerParagraph(time);
   clearInterval(timerId);
 });
 
+SOUND_TEST_BUTTON.addEventListener('click', () => {
+  AUDIO_FILE.play();
+});
 
 /**
  * Update Timer Paragraph
